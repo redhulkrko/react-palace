9da5148d67ee81d6195763c1cbbf4d047702809a
anfieldoldblueyes <old_blueyes@hotmail.com>
2020-05-12 20:57:21 +0100

    interim commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,5 @@
-const router = require('express').Router();
-const bookRoutes = require('./books');
-const path = require('path');
+import userRoutes from './user';
+import sessionRoutes from './session';
+import movieRoutes from './movie';
 
-// API routes
-router.use('/api/books', bookRoutes);
-
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-module.exports = router;
+export { userRoutes, sessionRoutes, movieRoutes};
