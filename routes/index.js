56429ba9646599a95e2df588e0f2ff239c96724e
anfieldoldblueyes <old_blueyes@hotmail.com>
2020-05-12 22:18:58 +0100

    update routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,5 @@
-const router = require('express').Router();
-const path = require('path');
-
 import userRoutes from './user';
 import sessionRoutes from './session';
 import movieRoutes from './movie';
-
-router.use(function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 export { userRoutes, sessionRoutes, movieRoutes};
--- server.js
+++ server.js
@@ -42,6 +42,20 @@ apiRouter.use('/users', userRoutes);
 apiRouter.use('/session', sessionRoutes);
 apiRouter.use('/movies', movieRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// for local env, use /client/public folder
+else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+}
+
+// server the client index.html file for all requests
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 } catch (err) {
   console.log(err)
