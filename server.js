933cac2afdf17806a6784f8736c89d76698e7870
anfieldoldblueyes <old_blueyes@hotmail.com>
2020-05-12 22:41:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -42,20 +42,10 @@ apiRouter.use('/users', userRoutes);
 apiRouter.use('/session', sessionRoutes);
 apiRouter.use('/movies', movieRoutes);
 const path = require('path');
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-}
-
-else {
   app.use(express.static(path.join(__dirname, '/client/public')));
   app.get("/*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/public/index.html"));
   });
-}
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 } catch (err) {
