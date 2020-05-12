732c719f5f366d880d2dcaf5eed7c2cba3664d9b
anfieldoldblueyes <old_blueyes@hotmail.com>
2020-05-12 21:29:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const {
     PORT = 5000,
     NODE_ENV = 'development',
-    MONGO_URI = 'mongodb://heroku_njw97gng:Logan417897@ds157204.mlab.com:57204/heroku_njw97gng',
+    MONGO_URI = 'mongodb://redhulkrko:silver990@ds157204.mlab.com:57204/heroku_njw97gng',
     SESS_NAME = 'sid',
     SESS_SECRET = 'secret!session',
     SESS_LIFETIME = 1000 * 60 * 60 * 2
