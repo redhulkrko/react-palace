f1d2c0e52332380b2d5898729024e0cac06e8c88
anfieldoldblueyes <old_blueyes@hotmail.com>
2020-06-07 16:53:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -41,10 +41,6 @@ app.use('/api', apiRouter);
 apiRouter.use('/users', userRoutes);
 apiRouter.use('/session', sessionRoutes);
 apiRouter.use('/movies', movieRoutes);
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-// });
-
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
