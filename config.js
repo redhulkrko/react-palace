8e22d3ed1e3ee1fcc6dfcf64d10a422a3327badc
anfieldoldblueyes <old_blueyes@hotmail.com>
2020-07-23 23:20:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const {
     PORT = 5000,
     NODE_ENV = 'development',
-    MONGO_URI = 'mongodb+srv://testUser:silver990$@cluster0-3ruts.mongodb.net/data?retryWrites=true&w=majority',
+    MONGO_URI = 'mongodb://redhulkrko:silver990@ds157204.mlab.com:57204/heroku_njw97gng',
     // MONGO_URI = 'mongodb://127.0.0.1:27017/data',
     SESS_NAME = 'sid',
     SESS_SECRET = 'secret!session',
