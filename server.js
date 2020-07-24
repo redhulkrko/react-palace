5d618d9fd2f59f2c6bd7e0247225b06f304c90db
anfieldoldblueyes <old_blueyes@hotmail.com>
2020-07-24 11:34:59 +0100

    Revert "Update server.js"
    This reverts commit 4e74f9dadf7f32c62b3f67735249aa3ddc0a7e85.

--- server.js
+++ server.js
@@ -42,9 +42,9 @@ app.use('/api', apiRouter);
 apiRouter.use('/users', userRoutes);
 apiRouter.use('/session', sessionRoutes);
 apiRouter.use('/movies', movieRoutes);
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 
 if (process.env.NODE_ENV === "production") {
