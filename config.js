b98ac8d029fd9738d2bf00b2c0e1eba0777ac1bf
anfieldoldblueyes <old_blueyes@hotmail.com>
2020-07-24 09:53:29 +0100

    heroku server

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const {
-    // PORT = 5000,
-    NODE_ENV = 'production',
+    PORT = 5000,
+    NODE_ENV = 'development',
     MONGO_URI = 'mongodb://redhulkrko:silver990@ds157204.mlab.com:57204/heroku_njw97gng',
     // MONGO_URI = 'mongodb://127.0.0.1:27017/data',
     SESS_NAME = 'sid',
