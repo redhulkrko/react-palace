964b2d65cdcf6f15232d1b815850693b4ac9f124
anfieldoldblueyes <old_blueyes@hotmail.com>
2020-07-24 11:35:07 +0100

    Revert "Update server.js"
    This reverts commit b3630ffe48ce4d5ae0c8bba1cab5707a82653b5c.

--- server.js
+++ server.js
@@ -47,19 +47,19 @@ app.get('*', (req, res) => {
 });
 
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("/*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//   });
+// }
 
-else {
-  app.use(express.static(path.join(__dirname, '/client/public')));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
-  });
-}
+// else {
+//   app.use(express.static(path.join(__dirname, '/client/public')));
+//   app.get("/*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/public/index.html"));
+//   });
+// }
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 } catch (err) {
